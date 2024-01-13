2761759adebdda2fe1759c893f78a8bc64ea59a0
luisgg121 <luisgg12_1@hotmail.com>
2024-01-13 10:04:40 -0600

    Commit

--- basedirectory/pages/index.js
+++ basedirectory/pages/index.js
@@ -3,7 +3,7 @@ function IndexPage() {
     return (
             <div>
                 <p>
-                    ¡Este es mi primer deploy con Next.js, de verdad!
+                    ¡Este es mi primer deploy con Next.js, de verdad2!
                 </p>
             </div>
     )
